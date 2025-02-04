7356751e4ba44e406676807d4ef2325ef422db67
DevAyush01 <Ayushchoudharry00@gmail.com>
2025-02-04 17:08:00 +0530

    about us section slider integrate

--- src/app/components/AboutSection.js
+++ src/app/components/AboutSection.js
@@ -1,34 +1,121 @@
-"use client"
-import { motion } from "framer-motion"
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import Target from "../../../public/Target.png";
+import { MorphingText } from "../../components/ui/morphing-text";
+import { InteractiveHoverButton } from "@/components/ui/interactive-hover-button";
+import AboutSliderImage1 from '../../../public/AboutSliderImage1.jpg'
 
-function AboutSection() {
+// Swiper Import 
+
+import "swiper/css"
+import "swiper/css/navigation"
+
+import { Swiper, SwiperSlide } from "swiper/react"
+import { Navigation, Pagination } from "swiper/modules"
+
+const morphingText = [
+  "Shaping the Future",
+  "Unlocking Potential",
+  "Building Excellence",
+  "Transforming Industries",
+  "Creating Impact",
+];
+
+const sliderImages = [AboutSliderImage1,AboutSliderImage1,AboutSliderImage1,AboutSliderImage1]
+
+export default function AboutSection() {
   return (
-    <div className="max-w-7xl mx-auto bg-red-300 py-24 px-4">
-      <div className="grid place-content-center bg-emerald-950 px-4 py-24 text-yellow-50">
-        <h1 className="max-w-2xl text-center text-5xl leading-snug">
-          Saroj{" "}
-          <span className="relative">
-            Educational
-            <svg viewBox="0 0 286 73" fill="none" className="absolute -left-2 -right-2 -top-2 bottom-0 translate-y-1">
-              <motion.path
-                initial={{ pathLength: 0 }}
-                whileInView={{ pathLength: 1 }}
-                transition={{
-                  duration: 1.25,
-                  ease: "easeInOut",
-                }}
-                d="M142.293 1C106.854 16.8908 6.08202 7.17705 1.23654 43.3756C-2.10604 68.3466 29.5633 73.2652 122.688 71.7518C215.814 70.2384 316.298 70.689 275.761 38.0785C230.14 1.37835 97.0503 24.4575 52.9384 1"
-                stroke="#FACC15"
-                strokeWidth="3"
-              />
-            </svg>
-          </span>{" "}
-          Group
-        </h1>
+    <>
+      <div className="w-full mx-auto ">
+        <div className="heading bg-orange-300 mx-auto text-center items-center max-w-7xl flex py-4 flex-row w-full">
+          <h1 className="text-6xl w-11/12">
+            Welcome to Saroj Educational Group
+          </h1>
+
+          <motion.div
+            animate={{
+              y: [0, -15, 0],
+            }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "loop",
+              duration: 3,
+              ease: "easeInOut",
+            }}
+          >
+            <Image src={Target} alt="Target" width={80} height={80} />
+          </motion.div>
+        </div>
+
+       {/* The About us and the Slider  */}
+       <div className="flex flex-row max-w-7xl mx-auto">
+
+
+         {/* LEFT SECTION  */}
+         <div className="flex flex-col items-start  justify-center bg-yellow-300 w-3/5 p-2 py-6">
+          <div>
+            <h1 className="text-4xl">Empowering Future Leaders,</h1>
+          </div>
+
+          <div className="w-full items-center ">
+            <MorphingText texts={morphingText} />
+          </div>
+
+          <div className="content w-3/4">
+            Welcome to Saroj Educational Group (SEG), established in 1997. Over
+            the years, SEG has grown exponentially, earning recognition both in
+            the industry and academia for its commitment to excellence. At SEG,
+            we pride ourselves on fostering a value-driven culture and providing
+            a professional environment where students thrive. As a diversified
+            institution, SEG offers cutting-edge education across multiple
+            disciplines, including engineering, management, law, pharmacy, and
+            more. Our mission is to shape future leaders who are equipped to
+            drive change, innovate, and create lasting value in their respective
+            fields.
+          </div>
+
+          <div className="connect bg-red-400 w-3/4">
+            <h2>Join us at Saroj Educational Group, where the journey towards shaping the future begins. 🚀</h2>
+            <InteractiveHoverButton>Connect now</InteractiveHoverButton>
+          </div>
+        </div>
+ 
+
+        {/* RIGHT SLIDER SECTION  */}
+        <div className="w-2/5 bg-purple-400 "> 
+           <div className="glimpse w-2/3 mx-auto bg-blue-300"><p className="text-5xl ">Here are </p> <p className="text-xl text-center"> Some College Glimpses</p></div>
+           <div className="slider ">
+           <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        autoplay={5000}
+        loop={true}
+        navigation
+        modules={[Navigation]}
+        // className=" xl:h-[80vh] lg:h-[60vh] md:h-[50vh] h-[30vh] w-full"
+        className="h-[80vh] absolute right-28"
+        >
+        {sliderImages.map((images,index) => (
+          <SwiperSlide key={index} className="relative w-full h-full mr-48">
+            <Image
+              src={images}
+              alt={`About Slider ${index}`}
+              layout="fill"
+              objectFit="cover"
+              priority={index === 0}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+           </div>
+        </div>
+     
+       </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default AboutSection
 
