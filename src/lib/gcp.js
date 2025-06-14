c707e2a4e65e28e7dceccde23b86667d876996b5
DevAyush01 <Ayushchoudharry00@gmail.com>
2025-06-14 14:56:44 +0530

    gcp issue fixing

--- src/lib/gcp.js
+++ src/lib/gcp.js
@@ -1,10 +1,21 @@
-// lib/gcp.js
 import { Storage } from "@google-cloud/storage";
-import path from "path";
 
-const storage = new Storage({
-  keyFilename: path.join(process.cwd(), process.env.GOOGLE_APPLICATION_CREDENTIALS),
-});
+let storage;
+if (process.env.NODE_ENV === 'production') {
+  // For production 
+  storage = new Storage({
+    projectId: process.env.GCP_PROJECT_ID,
+    credentials: {
+      client_email: process.env.GCP_CLIENT_EMAIL,
+      private_key: process.env.GCP_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    },
+  });
+} else {
+  // For local development
+  storage = new Storage({
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+  });
+}
 
 const bucket = storage.bucket(process.env.GCP_BUCKET_NAME);
 
